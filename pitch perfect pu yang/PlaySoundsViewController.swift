6bdf75708e4e71f343c4633282d0090c05b498f1
pu yang <yangpu2007360@gmail.com git config --global user.name Pu>
2018-01-21 12:28:01 -0800

    Added AV Foundation and record button

---
+++ pitch perfect pu yang/PlaySoundsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PlaySoundsViewController.swift
+//  pitch perfect pu yang
+//
+//  Created by pu yang on 1/21/18.
+//  Copyright © 2018 pu yang. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
