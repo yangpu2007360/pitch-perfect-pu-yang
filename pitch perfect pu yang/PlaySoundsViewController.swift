67f615812060805ecc0f7ff06529608246652204
pu yang <yangpu2007360@gmail.com git config --global user.name Pu>
2018-01-22 09:29:55 -0800

    finished

--- pitch perfect pu yang/PlaySoundsViewController.swift
+++ pitch perfect pu yang/PlaySoundsViewController.swift
@@ -53,11 +53,13 @@ class PlaySoundsViewController: UIViewController {
          }
         
     @IBAction func stopButtonPressed(_ sender: Any) {
+        stopAudio()
+        configureUI(.notPlaying)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
         // Do any additional setup after loading the view.
     }
 
